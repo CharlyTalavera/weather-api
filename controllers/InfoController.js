e932804da8dcb7465a3d7e2a70d718ab3eb758dd
Charly Talavera <talaveracarlosd@gmail.com>
2020-01-23 09:33:34 -0300

    format name added to response in info controller

--- controllers/InfoController.js
+++ controllers/InfoController.js
@@ -9,7 +9,7 @@ module.exports = {
     'GET /location': async (req, res) => {
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress.split(":").pop();
         const ipInfo = await IpService.getLocation(ip);
-
+        ipInfo.name = `${ipInfo.city}, ${ipInfo.regionName}, ${ipInfo.country}`
         return res.json(ipInfo);
     }
 
